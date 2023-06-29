9e13cb5b8435801dc002ccbe3912c6db6bfd4c4f
nemish493 <nemish.kyada@stud.th-deg.de>
2023-06-30 02:00:26 +0530

    new file:   .vscode/settings.json 	modified:   src/backend.js 	modified:   src/backend.json

--- src/backend.js
+++ src/backend.js
@@ -33,8 +33,8 @@ let park ='';
 let mobile_number='';
 let email_id='';
 let payment_option='';
-let ticket_print='';
-let edit='';
+let message='';
+
 
 io.on('connection', (socket) => {
   console.log('connected');
@@ -53,10 +53,10 @@ io.on('connection', (socket) => {
         moviename(socket,input);
         break;
       case 'date':
-        date_int(socket,input);
+        validateDate(socket,input);
         break;
       case 'time':
-        time_int(socket,input);
+        validateTime(socket,input);
         break;
       case 'seat':
         seat_Selection(socket,input);
@@ -65,7 +65,7 @@ io.on('connection', (socket) => {
         movie_lang(socket,input);
         break;
       case 'movie-type':
-        movie_type(socket,input);
+        validateMovieType(socket,input);
         break;
       case 'seat-number':
         seat_number(socket,input);
@@ -92,12 +92,9 @@ io.on('connection', (socket) => {
         payment(socket,input);
         break;
       case 'Ticket':
-        ticket(socket,input);
+        ticket(socket);
         break;
-      case 'edit':
-        change(socket,input);
-        break;
-
+      
     }
   });
 
@@ -123,9 +120,8 @@ function book(socket,input){
     intent ="moviename";
     socket.emit('botmes', intents[1].answers[0]);
   } else {
-    fallback();
+    handleBookFallback(socket, input);
   }
-
 }
 function moviename(socket,input){
   movieName=input;
@@ -134,89 +130,283 @@ function moviename(socket,input){
   console.log(movieName);
 
 }
-function date_int(socket,input){
-  date=input;
-  intent="time"
-  socket.emit('botmes',intents[3].answers);
-  console.log(date);
+function validateDate(socket, input) {
+  const currentDate = new Date().toISOString().split('T')[0];
+  const selectedDate = input.trim();
 
+  if (selectedDate.match(/^\d{4}-\d{2}-\d{2}$/)) {
+    if (selectedDate >= currentDate) {
+      date = selectedDate;
+      intent = 'time';
+      socket.emit('botmes', intents[3].answers);
+      console.log(date);
+    } else {
+      socket.emit('botmes', 'Please provide a valid date. Date should not be in the past.');
+    }
+  } else {
+    socket.emit('botmes', 'Please provide a valid date in the format YYYY-MM-DD.');
+  }
 }
-function time_int(socket,input){
-  time=input;
-  intent='seat'
-  socket.emit('botmes',intents[4].answers);
+function validateTime(socket, input) {
+  const timeRegex12Hour = /^(1[0-2]|0?[1-9]):([0-5][0-9])\s?(am|pm)$/i;
+  const timeRegex24Hour = /^([01]?[0-9]|2[0-3]):([0-5][0-9])$/;
   
-  console.log(time);
-  
+
+  if (input.match(timeRegex12Hour)) {
+   
+    const [_, hour, minutes, period] = input.match(timeRegex12Hour);
+    const isAM = period.toLowerCase() === 'am';
+    const hour24 = isAM ? (hour === '12' ? '00' : hour) : String(Number(hour) + 12);
+    time = `${hour24}:${minutes}`;
+  } else if (input.match(timeRegex24Hour)) {
+    time = input;
+  }
+
+  if (time) {
+    intent='seat'
+    socket.emit('botmes',intents[4].answers);
+    
+    console.log(time);
+    
+  } else {
+    socket.emit('botmes', 'Please provide a valid time in 24-hour format or 12-hour format (e.g., 13:00 or 1:00 PM).');
+  }
 }
-function seat_Selection(socket,input){
-  seats=input;
-  intent='movie-language'
-  socket.emit('botmes',intents[5].answers);
-  console.log(seats);
 
+function seat_Selection(socket, input) {
+  let match = intents[4].keywords.filter((keyword) => input.includes(keyword));
 
+  if (match.length !== 0) {
+    selectSeatType(socket, input);
+    
+  } else {
+    socket.emit('botmes', 'Please specify a valid seat type: Premium, General, or Recliner.');
+  }
 }
 function movie_lang(socket,input){
+  let match = intents[5].keywords.filter((keyword) => input.includes(keyword));
   movielan=input;
-  intent='movie-type'
-  socket.emit("botmes",intents[6].answers);
+  if (match.length !== 0) {
+    select_movie(socket, input);
+    
+  } else {
+    socket.emit('botmes', 'please specify valid language : English , Hindi , Deutsch');
+  }
 }
-function movie_type(socket,input){
-  movietype=input;
-  intent='seat-number'
-  socket.emit('botmes',intents[7].answers[0]);
+function validateMovieType(socket, input) {
+  
+  
+  let match = intents[6].keywords.filter((keyword) => input.includes(keyword));
+
+  if (match.length !== 0) {
+    movietype = match.toString();
+    intent='seat-number'
+    socket.emit('botmes',intents[7].answers[0]);
+    console.log(movietype)
+    
+  } else {
+    socket.emit('botmes', 'Please enter a valid movie type (3D or 2D).');
+  }
 }
+
 function seat_number(socket,input){
-  seatnumber=input;
+  seatnumber=parseInt(input);
+  if (isNaN(seatnumber)) {
+    socket.emit('botmes', 'Please specify seat number you want (Example: 123 ,023,32');
+    return;
+  }
   intent='total-adults'
   socket.emit('botmes',intents[8].answers);
+  console.log(seatnumber);
 
 }
-function adults(socket,input){
-  total_adults=input;
-  intent='total-children'
-  socket.emit('botmes',intents[9].answers);
+function adults(socket, input) {
+  total_adults = parseInt(input);
+  
+  if (isNaN(total_adults)) {
+    socket.emit('botmes', 'Please enter a valid number for the total number of adults.');
+    return;
+  }
+  
+  intent = 'total-children';
+  socket.emit('botmes', intents[9].answers);
+  console.log(total_adults);
 }
 function childeren(socket,input){
-  total_childeren=input;
+  total_childeren=parseInt(input);
+  if (isNaN(total_childeren)) {
+    socket.emit('botmes', 'Please enter a valid number for the total number of childeren.');
+    return;
+  }
   intent='food-service'
   socket.emit('botmes',intents[10].answers);
+  console.log(total_childeren);
 }
-function food(socket,input){
-  foods=input;
-  intent='parking-service'
-  socket.emit('botmes',intents[11].answers);
+function food(socket, input) {
+  let match = intents[10].keywords.filter((keyword) => input.includes(keyword));
+
+  if (match.length !== 0) {
+    foods = match[0]; 
+    intent = 'parking-service';
+    socket.emit('botmes', intents[11].answers);
+    console.log(foods);
+  } else {
+    socket.emit('botmes', 'Please select a valid food combo from the menu (fries, Burger, popcorn).');
+  }
 }
-function parking(socket,input){
-  park=input;
-  intent='mobile-number'
-  socket.emit('botmes',intents[12].answers);
+function parking(socket, input) {
+  let match = intents[11].keywords.filter((keyword) => input.includes(keyword));
+
+  if (match.length !== 0) {
+    park = 'Parking reserved for you.';
+  } else {
+    park = ' parking not needed';
+  }
+
+  intent = 'mobile-number';
+  socket.emit('botmes', intents[12].answers);
 }
-function mobile(socket,input){
-  mobile_number=input;
-  intent='email-id'
-  socket.emit('botmes',intents[13].answers);
+function mobile(socket, input) {
+  const numberRegex = /\d+/g;
+  const numbers = input.match(numberRegex);
+
+  if (!numbers || numbers.length === 0 || numbers[0].length < 10) {
+    socket.emit('botmes', 'Please enter a valid mobile number.');
+    return;
+  }
+ 
+
+  mobile_number = numbers[0];
+  console.log(mobile_number);
+  intent = 'email-id';
+  socket.emit('botmes', intents[13].answers);
 }
-function email(socket,input){
-  email_id=input;
-  intent='payment-option'
-  socket.emit('botmes',intents[14].answers);
+function email(socket, input) {
+  const emailRegex = /\S+@\S+\.\S+/;
+
+  if (!emailRegex.test(input)) {
+    socket.emit('botmes', 'Please enter a valid email address.');
+    return;
+  }
+
+  email_id = input;
+  console.log(email_id);
+  intent = 'payment-option';
+  socket.emit('botmes', intents[14].answers);
 }
-function payment(socket,input){
-  payment_option=input;
-  intent='Ticket'
-  socket.emit('botmes',intents[15].answers);
+
+function payment(socket, input) {
+  
+
+  
+  const match = intents[14].keywords.find(keyword => input.toLowerCase().includes(keyword));
+
+  if (match) {
+    payment_option = match;
+    const confirmation= `your payment option is ${payment_option}`;
+    socket.emit('botmes',confirmation);
+  
+  } else {
+    socket.emit('botmes', 'Please select a valid payment option (cash or online).');
+    return;
+  }
+  console.log(payment_option);
+
+  intent = 'Ticket';
+  socket.emit('botmes', intents[15].answers);
+  ticket(socket);
 }
-function ticket(socket,input){
-  ticket_print=input;
-  intent='edit'
+
+function ticket(socket) {
+  const seatPrice = 10; 
+  const totalCost = total_adults * seatPrice + total_childeren * 1.5;
+  
+  message = `Ticket Details:
+  Movie Name: ${movieName}
+  Date: ${date}
+  Time: ${time}
+  Seats: ${seats}
+  Language: ${movielan}
+  Movie Type: ${movietype}
+  Seat Number: ${seatnumber}
+  Food: ${foods}
+  Parking Option: ${park}
+  Total Cost: ${totalCost}
+  Mobile Number: ${mobile_number}
+  Email: ${email_id}`;
+  console.log(message);
+
+  socket.emit('botmes', message);
+  
   socket.emit('botmes',intents[16].answers);
 }
-function change(socket,input){
-  edit=input;
-  console.log(edit);
-}
+
+
 function fallback() {
   // Fallback logic goes here
 }
+
+
+function handleBookFallback(socket, input) {
+  // Check if the input contains any keywords related to "yes" or "no"
+  let yesMatch = intents[1].keywords.filter((keyword) =>
+    input.includes(keyword)
+  );
+  
+
+  if (yesMatch.length == 0 ) {
+    // User provided a response, but it wasn't clear. Ask them to rephrase.
+    socket.emit('botmes', "Sorry, I didn't understand your response. Please rephrase it.");
+  } else {
+    fallback();
+  }
+}
+function selectSeatType(socket, input) {
+  let seatType = '';
+  let seatPrice = 0;
+
+  if (input.includes('premium')) {
+    seatType = 'Premium Seat';
+    seatPrice = 2;
+  } else if (input.includes('general')) {
+    seatType = 'General Seat';
+    seatPrice = 1.5;
+  } else if (input.includes('recliner')) {
+    seatType = 'Recliner Seat';
+    seatPrice = 4.5;
+  }
+
+  if (seatType !== '' && seatPrice !== 0) {
+    seats = `${seatType}: €${seatPrice}`;
+    intent = 'movie-language';
+    socket.emit('botmes', intents[5].answers);
+  } else {
+    socket.emit('botmes', 'Please specify a valid seat type: Premium, General, or Recliner.');
+  }
+  console.log(seats);
+  
+}
+function select_movie(socket, input) {
+  let selectedmovie = '';
+  
+
+  if (input.includes('english')) {
+    selectedmovie = 'english'
+  } else if (input.includes('hindi')) {
+    selectedmovie = 'hindi'
+  } else if (input.includes('deutsch','german')) {
+    selectedmovie = 'deutsch'
+  }
+
+  if (selectedmovie !== '' ) {
+    movie_lang = `${selectedmovie}`;
+    intent = 'movie-type';
+    socket.emit('botmes', intents[6].answers);
+  } else {
+    socket.emit('botmes', 'Please specify a valid language from: English , hindi , German');
+  }
+  console.log(movie_lang);
+}
+
+
+
